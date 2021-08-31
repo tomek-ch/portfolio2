9f9b7ffc6add1cdefc8aa63a68a2d63cab9871d9
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-08-31 19:37:24 +0200

    Add route indicator to header

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,26 @@
 import Link from "next/link";
-import { header } from "../styles/Header.module.css";
+import { header, activeLink } from "../styles/Header.module.css";
+import { useRouter } from "next/router";
 
-const Header = () => (
-  <header className={header}>
-    <h1>
-      <Link href="/">tomek-ch</Link>
-    </h1>
-    <nav>
-      <Link href="/">home</Link>
-      <Link href="/projects">projects</Link>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const router = useRouter();
+  return (
+    <header className={header}>
+      <h1>
+        <Link href="/">tomek-ch</Link>
+      </h1>
+      <nav>
+        <Link href="/">
+          <a className={router.asPath === "/" ? activeLink : ""}>home</a>
+        </Link>
+        <Link href="/projects">
+          <a className={router.asPath === "/projects" ? activeLink : ""}>
+            projects
+          </a>
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
