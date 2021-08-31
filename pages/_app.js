6f93c19a5a82e98d8ebc70979e73310c223026af
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-08-31 16:54:10 +0200

    Add page wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "../styles/globals.css";
 import Header from "../components/Header";
+import { wrapper } from "../styles/Wrapper.module.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <div className={wrapper}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
 
