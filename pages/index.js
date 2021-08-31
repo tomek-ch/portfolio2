a03f4ad2b47386defebdef731c8d3bcd6ede34fc
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-08-31 20:26:30 +0200

    Remove unused import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import HelloSection from "../components/HelloSection";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
