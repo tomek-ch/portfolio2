9566514b0096725c45fadb420d87bd5b9f07323a
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-08-31 18:04:44 +0200

    Set typeface

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import "../styles/globals.css";
 import Header from "../components/Header";
 import { wrapper } from "../styles/Wrapper.module.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className={wrapper}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </div>
