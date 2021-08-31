048ac27c17c84a95069231a3ce5b6c17cc1e1bb1
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-08-31 21:28:36 +0200

    Add heading to projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,14 +1,16 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { highlight } from "../styles/Highlight.module.css";
+import { heading } from "../styles/Heading.module.css";
 
-export default function Home() {
+export default function Projects() {
   return (
     <>
       <Head>
         <title>projects | tomek-ch</title>
       </Head>
-      <h1>Projects</h1>
+      <h1 className={heading}>
+        Some of my personal <span className={highlight}>projects</span>:
+      </h1>
     </>
   );
 }
